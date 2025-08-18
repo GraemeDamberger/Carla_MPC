7a5cae1661787a7d7c54601e6ada5fd70db5aea0
unknown <graemedamberger@gmail.com>
2025-08-18 17:13:48 -0400

    tightened steering constraints

--- Experiments/Tube/config.py
+++ Experiments/Tube/config.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 
 config = {
 # Model and training
-    "samples": 200000,
+    "samples": 500000,
     "batch_size": 64,
-    "epochs": 200,
+    "epochs": 1000,
     "data_path":"Data/Training_Data/training_set.npy",
     "model_path":"Data/model.pth",
     "weight_decay": 2e-3, #5e-2
@@ -15,8 +15,8 @@ config = {
 # Controller
     "Np": 50, #50
     "N": 5,
-    "M_u_lb":-np.pi/2.5,
-    "M_u_ub":np.pi/2.5,
+    "M_u_lb":-np.pi/10,
+    "M_u_ub":np.pi/10,
     "Q": 1e1,
     "R": 0,
     "kpV":100,
@@ -32,7 +32,7 @@ config = {
     "sim_T": 10,
     "ref_steps":100,
     "num_trials":1,
-    "steps": 5000,
+    "steps": 10000,
     "ref_points":1500
 }
 '''
